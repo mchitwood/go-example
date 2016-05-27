ff6134770012c537ee1ddcacc7fb7799c6808183
Marc Chitwood <Marc Chitwood>
2016-05-26 20:24:02 -0500

    Debug

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-    http.HandleFunc("/", hello)
-    #err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-    #if err != nil {
-    #    log.Fatal("ListenAndServe:", err)
-    #}
+// Movie Struct
+type Movie struct {
+	Title  string `json:"title"`
+	Rating string `json:"rating"`
+	Year   string `json:"year"`
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(w, "hello, world!")
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/movies", handleMovies).Methods("GET")
+	http.ListenAndServe(os.Getenv("OPENSHIFT_GO_IP") + ":" + os.Getenv("OPENSHIFT_GO_PORT"), router)
+}
+
+func handleMovies(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+
+	var movies = map[string]*Movie{
+		"tt0076759": &Movie{Title: "Star Wars: A New Hope", Rating: "8.7", Year: "1977"},
+		"tt0082971": &Movie{Title: "Indiana Jones: Raiders of the Lost Ark", Rating: "8.6", Year: "1981"},
+	}
+
+	outgoingJSON, error := json.Marshal(movies)
+
+	if error != nil {
+		log.Println(error.Error())
+		http.Error(res, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(res, string(outgoingJSON))
 }
